7548b419764a35b2c92585817187751944f5d3c2
Chi Nguyen <chi.nguyen@live.com>
2023-01-13 15:07:21 +0200

    list views

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, SafeAreaView} from 'react-native';
+import List from './components/List';
 
-export default function App() {
+const App = () => {
+  //console.log('App starting!')
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+      <SafeAreaView style={styles.container}>
+        <List />
+      </SafeAreaView>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
