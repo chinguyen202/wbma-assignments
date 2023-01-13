2aab05717e7465f61680ce3e533c327bb6e11f08
Chi Nguyen <chi.nguyen@live.com>
2023-01-13 15:24:24 +0200

    task b

--- components/List.js
+++ components/List.js
@@ -1,16 +1,26 @@
 import {useEffect, useState} from 'react';
 import {FlatList, StyleSheet} from 'react-native';
+import {baseUrl} from '../utils/variables';
 import ListItem from './ListItem';
 
 const List = () => {
-  const url =
-    'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
   const [mediaArray, setMediaArray] = useState([]);
 
   const loadMedia = async () => {
-    const response = await fetch(url);
-    const json = await response.json();
-    setMediaArray(json);
+    try {
+      const response = await fetch(baseUrl + 'media');
+      const json = await response.json();
+      const media = await Promise.all(
+        json.map(async (file) => {
+          const fileResponse = await fetch(baseUrl + 'media/' + file.file_id);
+          return await fileResponse.json();
+        })
+      );
+
+      setMediaArray(media);
+    } catch (error) {
+      console.error('List, loadMedia', error);
+    }
   };
 
   useEffect(() => {
@@ -22,6 +32,7 @@ const List = () => {
   return (
     <FlatList
       data={mediaArray}
+      keyExtractor={(item, index) => index.toString()}
       renderItem={({item}) => <ListItem singleMedia={item} />}
     />
   );
