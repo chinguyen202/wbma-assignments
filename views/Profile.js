859653e87deedf25a96afa513bfeef734133d9a2
Chi Nguyen <chi.nguyen@live.com>
2023-01-22 15:33:30 +0200

    add basic user profile

--- views/Profile.js
+++ views/Profile.js
@@ -4,19 +4,23 @@ import {MainContext} from '../contexts/MainContext';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Profile = () => {
-  const {setIsLoggedIn} = useContext(MainContext);
+  const {setIsLoggedIn, user, setUser} = useContext(MainContext);
   return (
     <SafeAreaView style={styles.container}>
       <Text>Profile</Text>
+      <Text>Username: {user.username}</Text>
+      <Text>Email: {user.email}</Text>
+      <Text>Full name: {user.full_name}</Text>
       <Button
         title="Logout!"
         onPress={async () => {
           console.log('Logging out!');
+          setUser({});
           setIsLoggedIn(false);
           try {
             await AsyncStorage.clear();
           } catch (error) {
-            console.error('clearing async storage failed', error);
+            console.error('clearing asyncstorage failed', error);
           }
         }}
       />
